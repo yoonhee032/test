77cd5742d05fc923feff4267035f033051c399e4
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-02-01 23:52:33 +0900

    asuync await 추가

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,10 +1,14 @@
-export default function signup(req, res) {
+import connectMongo from "../../../utils/connectMongo";
+
+export default async function signup(req, res) {
   console.log("요청이 들어왔습니다. ", req);
   const date = new Date();
   const format = date.toGMTString();
 
   if (req.method === "POST") {
     console.log("포스트");
+
+    await connectMongo();
 
     res.json({
       status: true,
