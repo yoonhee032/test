85312a65055326b52082b684b7f7343b4f94bcf6
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-02-02 00:03:57 +0900

    .

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,19 +1,20 @@
 import connectMongo from "../../../utils/connectMongo";
 
-export default async function signup(req, res) {
-  console.log("요청이 들어왔습니다. ", req);
-  const date = new Date();
-  const format = date.toGMTString();
+export default async function hendler(req, res) {
+  const { method } = req;
 
-  if (req.method === "POST") {
-    console.log("포스트");
+  connectMongo();
 
-    await connectMongo();
+  if (method === "POST") {
+    try {
+      console.log("포스트");
 
-    res.json({
-      status: true,
-      now: format,
-      message: "회원가입을 했습니다.",
-    });
+      res.json({
+        status: true,
+        message: "회원가입을 했습니다.",
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
 }
